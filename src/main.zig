841802ca90031ac283a18829b2598b85a670c519
Vetle de Vries <vetle1608@gmail.com>
2023-08-27 13:10:55 +0200

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const dprint = std.debug.print;
 
 const u8arr_list: type = std.ArrayList(u8);
+const u8_slice: type = []const u8;
 
 fn print_cmds() !void {
     const writer = std.io.getStdOut().writer();
@@ -12,21 +13,22 @@ fn print_cmds() !void {
 
 const delimiter = '\n';
 
-fn read_input() u8arr_list {
+fn read_input() !void {
     const reader = std.io.getStdIn().reader();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var allocator = gpa.allocator();
-    defer allocator.deinit();
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
     var input = std.ArrayList(u8).init(allocator);
     defer input.deinit();
 
     try reader.streamUntilDelimiter(input.writer(), delimiter, 1024);
-
-    return input;
+    dprint("value: {s}\n", .{input.items});
 }
 
 pub fn main() !void {
     try print_cmds();
+    dprint("Enter a value: ", .{});
+    try read_input();
 }
