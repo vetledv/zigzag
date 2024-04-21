f609541050bc05389f54ce3a40b465c8af8a863c
Vetle de Vries Brandth <vetle@dvries.com>
2024-04-21 11:43:56 +0200

    asd

--- src/general.zig
+++ src/general.zig
@@ -3,7 +3,6 @@ const std = @import("std");
 const delimiter: u8 = '\n';
 const max_size: usize = 1024;
 
-///call input.deinit() to free the memory
 pub fn read_input(allocator: std.mem.Allocator) !std.ArrayList(u8) {
     const reader = std.io.getStdIn().reader();
     var arr_list = std.ArrayList(u8).init(allocator);
@@ -13,7 +12,6 @@ pub fn read_input(allocator: std.mem.Allocator) !std.ArrayList(u8) {
     return arr_list;
 }
 
-///call allocator.free(slice) to free the memory
 pub fn read_input_to_slice(allocator: std.mem.Allocator) ![]u8 {
     const reader = std.io.getStdIn().reader();
     var arr_list = std.ArrayList(u8).init(allocator);
--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,31 @@
 const std = @import("std");
 const general = @import("general.zig");
-const dprint = std.debug.print;
+const debugPrint = std.debug.print;
 
 fn print_cmds() !void {
     const writer = std.io.getStdOut().writer();
     try writer.print("{s}\n", .{"-" ** 32});
     try writer.print("Hello World!\n", .{});
-    //TODO: random commands
     try writer.print("{s}\n", .{"-" ** 32});
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    //error: value of type 'heap.general_purpose_allocator.Check' ignored
-    //all non-void values must be used
-    //this error can be suppressed by assigning the value to '_'
-    //TODO: git gud, I don't get this error
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
     try print_cmds();
 
-    dprint("Enter a value: ", .{});
+    debugPrint("Enter a value: ", .{});
 
     const input = try general.read_input(allocator);
     defer input.deinit();
 
-    dprint("You entered: {s}\n", .{input.items});
-    dprint("Enter another value: ", .{});
+    debugPrint("You entered: {s}\n", .{input.items});
+    debugPrint("Enter another value: ", .{});
 
     const slice = try general.read_input_to_slice(allocator);
     defer allocator.free(slice);
 
-    dprint("You entered: {s}\n", .{slice});
+    debugPrint("You entered: {s}\n", .{slice});
 }
